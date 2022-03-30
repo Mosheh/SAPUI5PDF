e630aa599d09cd8cdab9e48e65fd3931c12ee756
Moisés J. Miranda <moises.miranda@inventsoftware.com.br>
2022-03-30 17:29:53 -0300

    exemplo de código para extrair pdf ui5

---
+++ List.controller.js
@@ -0,0 +1,66 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/json/JSONModel",
+	"myApp/lib/jspdf"
+], function (Controller, Filter, FilterOperator, JSONModel, jsPDF) {
+	"use strict";
+
+	return Controller.extend("myApp.List", {
+
+		
+		onInit: function () {
+			// set explored app's demo model on this sample
+			var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
+			this.getView().setModel(oModel);
+
+			this.pdf = jspdf;
+		},
+
+		onPrint(){
+			console.log(this.pdf);
+
+			let img = new Image();
+			img.src="img/20190105_132952.jpg";
+
+			const doc = new this.pdf.jsPDF();
+			doc.text("Moisés", 10, 10);
+			doc.addImage(img, 'jpg', 100, 80, 185, 150, null, null, 45);
+			doc.save("MOSHEH.pdf");
+		},
+
+		onSearch: function (oEvent) {
+			// add filter for search
+			var aFilters = [];
+			var sQuery = oEvent.getSource().getValue();
+			if (sQuery && sQuery.length > 0) {
+				var filter = new Filter("Name", FilterOperator.Contains, sQuery);
+				aFilters.push(filter);
+			}
+
+			// update list binding
+			var oList = this.byId("idList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilters, "Application");
+		},
+
+		onSelectionChange: function (oEvent) {
+			var oList = oEvent.getSource();
+			var oLabel = this.byId("idFilterLabel");
+			var oInfoToolbar = this.byId("idInfoToolbar");
+
+			// With the 'getSelectedContexts' function you can access the context paths
+			// of all list items that have been selected, regardless of any current
+			// filter on the aggregation binding.
+			var aContexts = oList.getSelectedContexts(true);
+
+			// update UI
+			var bSelected = (aContexts && aContexts.length > 0);
+			var sText = (bSelected) ? aContexts.length + " selected" : null;
+			oInfoToolbar.setVisible(bSelected);
+			oLabel.setText(sText);
+		}
+
+	});
+});
